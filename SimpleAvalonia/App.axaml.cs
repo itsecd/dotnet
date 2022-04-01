8d4ec776634ae4aaaf6c689351c351248b8bfb35
Ruslan Yuzkiv <55447008+navna@users.noreply.github.com>
2022-04-01 11:37:15 +0400

    Remove partial from App

--- SimpleAvalonia/App.axaml.cs
+++ SimpleAvalonia/App.axaml.cs
@@ -4,7 +4,7 @@ using Avalonia.Markup.Xaml;
 
 namespace SimpleAvalonia
 {
-    public partial class App : Application
+    public class App : Application
     {
         public override void Initialize() =>
             AvaloniaXamlLoader.Load(this);
