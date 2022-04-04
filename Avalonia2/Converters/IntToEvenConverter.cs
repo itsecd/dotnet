692b645a3572cd96e2cfdebcaa6e9d3a29264b41
Ruslan Yuzkiv <55447008+navna@users.noreply.github.com>
2022-04-04 21:45:51 +0400

    Fix not-nullable return types for IntToEvenConverter

--- Avalonia2/Converters/IntToEvenConverter.cs
+++ Avalonia2/Converters/IntToEvenConverter.cs
@@ -7,10 +7,10 @@ namespace Avalonia2.Converters
 {
     public class IntToEvenConverter : IValueConverter
     {
-        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
             value is int intValue && intValue % 2 == 0;
 
-        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
             throw new NotImplementedException();
     }
 }
